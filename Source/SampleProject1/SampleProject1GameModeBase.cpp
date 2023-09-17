453065afdbbf930cf1228f9671b51cb70bd0f2fc
Chae Ho Shin <gshin3@live.com>
2023-09-17 22:45:27 +0900

    Initial Commit

---
+++ Source/SampleProject1/SampleProject1GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "SampleProject1GameModeBase.h"
+
---
+++ Source/SampleProject1/SampleProject1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SampleProject1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SAMPLEPROJECT1_API ASampleProject1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
