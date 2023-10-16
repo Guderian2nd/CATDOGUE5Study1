e2770005891526129bbb41aa874f181c7969e8cf
Chae Ho Shin <gshin3@live.com>
2023-10-17 06:33:52 +0900

    Homework 1 example solution

--- Source/SampleProject1/Private/RacingWidgetBase.cpp
+++ Source/SampleProject1/Private/RacingWidgetBase.cpp
@@ -2,3 +2,112 @@
 
 
 #include "RacingWidgetBase.h"
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Blueprint/SlateBlueprintLibrary.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Components/CanvasPanel.h"
+#include "Misc/Timespan.h"
+
+
+void URacingWidgetBase::SetArrowLoc_Implementation(bool IsWaypointOutOfScreen, FVector2D WaypointLocation)
+{
+	if (IsWaypointOutOfScreen)
+	{
+		double ScreenLocXOverViewportX, ScreenLocXOverViewportY = 0.0;
+		auto ArrowPosition = FVector2D{};
+		if (IsPointCloserToXAxis(WaypointLocation, ScreenLocXOverViewportX, ScreenLocXOverViewportY))
+		{
+			USlateBlueprintLibrary::ScreenToViewport(this, (1.0 / ScreenLocXOverViewportX) * WaypointLocation, ArrowPosition);
+		}
+		else
+		{
+			USlateBlueprintLibrary::ScreenToViewport(this, (1.0 / ScreenLocXOverViewportY) * WaypointLocation, ArrowPosition);
+		}
+
+		if (WaypointIndicatorArrow)
+		{
+			UWidgetLayoutLibrary::SlotAsCanvasSlot(WaypointIndicatorArrow)->SetPosition(ArrowPosition);
+
+			WaypointIndicatorArrow->SetRenderTransformAngle(FMath::RadiansToDegrees(atan2(WaypointLocation.Y, WaypointLocation.X)) - 270.0);
+		}
+	}
+	else
+	{
+		auto ArrowPosition = FVector2D{};
+
+		if (WaypointIndicatorArrow)
+		{
+			USlateBlueprintLibrary::ScreenToViewport(this, WaypointLocation, ArrowPosition);
+			UWidgetLayoutLibrary::SlotAsCanvasSlot(WaypointIndicatorArrow)->SetPosition(ArrowPosition);
+
+			WaypointIndicatorArrow->SetRenderTransformAngle(180.0);
+		}
+	}
+}
+
+void URacingWidgetBase::SetArrowVisibility_Implementation(bool IsVisible)
+{
+	if (WaypointIndicatorArrow)
+	{
+		WaypointIndicatorArrow->SetVisibility(IsVisible ? ESlateVisibility::HitTestInvisible : ESlateVisibility::Collapsed);
+	}
+}
+
+void URacingWidgetBase::SetTimerText_Implementation(float TimeInSeconds)
+{
+	if (TimerTextWidget)
+	{
+		SetTextTime(TimeInSeconds, TimerTextWidget);
+	}
+}
+
+void URacingWidgetBase::SetWaypointNum_Implementation(int AcquiredWaypointNum, int TotalWaypointNum)
+{
+	auto WaypointProgressString = FString::Printf(TEXT("%d / %d"), AcquiredWaypointNum, TotalWaypointNum);
+
+	if (WaypointProgressTextWidget)
+	{
+		WaypointProgressTextWidget->SetText(FText::FromString(WaypointProgressString));
+	}
+}
+
+void URacingWidgetBase::ToggleVictoryMessage_Implementation(bool IsRaceFinished, float FinalTime)
+{
+	if (VictoryCanvasPanel)
+	{
+		VictoryCanvasPanel->SetVisibility(IsRaceFinished ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+	}
+	if (RacingCanvasPanel)
+	{
+		RacingCanvasPanel->SetVisibility(IsRaceFinished ? ESlateVisibility::Collapsed : ESlateVisibility::Visible);
+	}
+	if (FinalTimeTextWidget && IsRaceFinished)
+	{
+		SetTextTime(FinalTime, FinalTimeTextWidget);
+	}
+}
+
+bool URacingWidgetBase::IsPointCloserToXAxis(FVector2D ScreenLocation, double& ScreenLocXOverViewportX, double& ScreenLocYOverViewportY)
+{
+	auto ViewportSize = FVector2D{};
+	GetWorld()->GetGameViewport()->GetViewportSize(ViewportSize);
+	ViewportSize = (ViewportSize / -2.0).GetAbs();
+
+	auto ScreenLocAbsVec = ScreenLocation.GetAbs();
+
+	ScreenLocXOverViewportX = ScreenLocAbsVec.X / ViewportSize.X;
+	ScreenLocYOverViewportY = ScreenLocAbsVec.Y / ViewportSize.Y;
+
+	return ScreenLocXOverViewportX > ScreenLocYOverViewportY;
+}
+
+void URacingWidgetBase::SetTextTime(double Seconds, UTextBlock* TextToSet)
+{
+	auto Mins = FMath::FloorToInt(FTimespan::FromSeconds(Seconds).GetTotalMinutes());
+	auto Secs = FTimespan::FromSeconds(Seconds).GetSeconds();
+	auto MilliSecs = FTimespan::FromSeconds(Seconds).GetFractionMilli();
+
+	TextToSet->SetText(FText::FromString(FString::Printf(TEXT("%02d:%02d.%03d"), Mins, Secs, MilliSecs)));
+}
--- Source/SampleProject1/Public/RacingWidgetBase.h
+++ Source/SampleProject1/Public/RacingWidgetBase.h
@@ -15,18 +15,77 @@ class SAMPLEPROJECT1_API URacingWidgetBase : public UUserWidget
 	GENERATED_BODY()
 	
 public:
-	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Controller Communication")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Controller Communication")
 	void SetArrowLoc(bool IsWaypointOutOfScreen, FVector2D WaypointLocation);
+	virtual void SetArrowLoc_Implementation(bool IsWaypointOutOfScreen, FVector2D WaypointLocation);
 
-	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Controller Communication")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Controller Communication")
 	void SetArrowVisibility(bool IsVisible);
+	virtual void SetArrowVisibility_Implementation(bool IsVisible);
 	
-	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Controller Communication")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Controller Communication")
 	void SetTimerText(float TimeInSeconds);
-	
-	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Controller Communication")
-	void SetWaypointNum(int AcquiredWaypointNum, int TotalWaypointNum);
+	virtual void SetTimerText_Implementation(float TimeInSeconds);
 
-	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent, Category = "Controller Communication")
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Controller Communication")
+	void SetWaypointNum(int AcquiredWaypointNum, int TotalWaypointNum);
+	virtual void SetWaypointNum_Implementation(int AcquiredWaypointNum, int TotalWaypointNum);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Controller Communication")
 	void ToggleVictoryMessage(bool IsRaceFinished, float FinalTime);
+	virtual void ToggleVictoryMessage_Implementation(bool IsRaceFinished, float FinalTime);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parent Class Properties")
+	/// <summary>
+	/// The arrow that points to on or off screen waypoints
+	/// </summary>
+	UWidget* WaypointIndicatorArrow = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parent Class Properties")
+	/// <summary>
+	/// Text that displays the current time elapsed since start of race
+	/// </summary>
+	class UTextBlock* TimerTextWidget = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parent Class Properties")
+	/// <summary>
+	/// Text that displays the final record after finishing the race
+	/// </summary>
+	UTextBlock* FinalTimeTextWidget = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parent Class Properties")
+	/// <summary>
+	/// Text Widget that displays how much of the race course you've run
+	/// </summary>
+	UTextBlock* WaypointProgressTextWidget = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parent Class Properties")
+	/// <summary>
+	/// Panel that holds information about the ongoing race
+	/// </summary>
+	class UCanvasPanel* RacingCanvasPanel = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parent Class Properties")
+	/// <summary>
+	/// Panel that displays when race is finished
+	/// </summary>
+	UCanvasPanel* VictoryCanvasPanel = nullptr;
+
+private:
+	/// <summary>
+	/// Checks whether an arrow from screen center to an out-of-screen location touches the sides or top/bottom of the screen
+	/// </summary>
+	/// <param name="ScreenLocation"></param>
+	/// <param name="ScreenLocXOverViewportX">Writes the ScreenLocX/ViewportX value to this variable</param>
+	/// <param name="ScreenLocYOverViewportY">Writes the ScreenLocY/ViewportY value to this variable</param>
+	/// <returns>True if the line from screen center to ScreenLocation touches the sides, false if it touches the top or bottom of the screen edge</returns>
+	bool IsPointCloserToXAxis(FVector2D ScreenLocation, double& ScreenLocXOverViewportX, double& ScreenLocYOverViewportY);
+
+	/// <summary>
+	/// Sets the text of a UTextBlock in mm:ss.xxx time format when given a time in seconds.
+	/// </summary>
+	/// <param name="Seconds"></param>
+	/// <param name="TextToSet"></param>
+	void SetTextTime(double Seconds, UTextBlock* TextToSet);
 };
